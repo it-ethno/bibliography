34b781b3e7b0d353dec8994e99395f6dff2e3126
IT-Ethno <thorsten-gries@gmx.de>
2022-08-12 00:05:39 +0200

    Added quite some settings HTML and some functions

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
+let settings = {
+    language: 'en',
+    useBackground: true,
+    changeInterval: 1,
+    useFixedBackground: false,
+    fixedBackground: '',
+    inputFormat: '',
+    outputFormat: '',
+    serverURL: ''
+};
+
 let imageReload = setInterval(loadRandomBackgroundImage, 60000);
 equipListeners();
 
 function loadRandomBackgroundImage() {
-    let randomNum = Math.round((Math.random() * 15) + 1);
-    console.log('Random Number: ' + randomNum);
-    document.querySelector('#alltainer').style.backgroundImage = "url('./images/background_" + randomNum + ".jpg')";
+    document.querySelector('#alltainer').style.backgroundImage = "url('./images/background_" + Math.round((Math.random() * 15) + 1) + ".jpg')";
 }
 
 function togglePane(e) {
-    let t = e.target.id.substring(12);
-    console.log('Target: ' + t);
-    let targetNode = document.querySelector('#'+t);
-    console.log('Visibility: ' + targetNode.style.visibility);
+    let targetNode = document.querySelector('#' + e.target.id.substring(12));
     targetNode.style.visibility === 'visible' ? targetNode.style.visibility = 'hidden' : targetNode.style.visibility = 'visible';
 }
 
 function equipListeners() {
-    let toggleNodes = document.querySelectorAll('.paneToggler');
-    toggleNodes.forEach( (node) => { 
-        node.addEventListener('click', togglePane);
-     });
-    console.log('Listeners equipped!');
+    document.querySelectorAll('.paneToggler').forEach( (node) => { node.addEventListener('click', togglePane); });
+    document.querySelector('#settingsToggler').addEventListener('click', toggleSettings);
+    document.querySelector('#closeSettings').addEventListener('click', toggleSettings);
+}
+
+function toggleSettings() {
+    let settings = document.querySelector('#settings');
+    settings.style.visibility === 'visible' ? settings.style.visibility = 'hidden' : settings.style.visibility = 'visible';
+}
+
+function handleSettingsChange(e) {
+    switch(e.target.id) {
+
+    }
+}
+
+function exportLocalDataToJsonFile() {
+    
 }
